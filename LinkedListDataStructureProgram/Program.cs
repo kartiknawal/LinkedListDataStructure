e70388963fe521ef0d3cc07fd33cb103797c8481
kartiknawal <kartiknawal.7@gmail.com>
2020-10-22 08:08:18 +0530

    [kartik] Add. initial commit

---
+++ LinkedListDataStructureProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDataStructureProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Data Structure Program!");
+        }
+    }
+}
